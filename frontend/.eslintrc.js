228a288eec1234a26ad4c97d5fe9677c13cc2e3f
Jonas Cosandey <jonascosandey@hotmail.com>
2017-12-07 23:03:06 +0100

    add frontend. remove django. replace with elixir

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	parserOptions: {
+	ecmaVersion: 2017,
+	sourceType: 'module'
+	},
+	parser: 'babel-eslint',
+	env: {
+	browser: true
+	},
+	plugins: ['ember', 'prettier'],
+	extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
+	rules: {
+	'prettier/prettier': 'error'
+	}
+	}
---
+++ frontend/ember-cli-build.js
@@ -0,0 +1,18 @@
+/* global require, module */
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
+
+module.exports = function (defaults) {
+const app = new EmberApp(defaults, {
+sassOptions: {
+includePaths: [ 'node_modules' ]
+},
+babel: {
+optional: ['es7.asyncFunctions', 'es7.decorators']
+}
+})
+
+app.import('node_modules/moment/locale/de.js')
+
+return app.toTree()
+}
